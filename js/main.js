1b7e650a7e13f451bf774d770e075dec22c1ac16
Roargut <roargut@gmail.com>
2016-03-29 20:39:22 +0200

    update

--- js/main.js
+++ js/main.js
@@ -1,12 +1,28 @@
 /**
  * Created by RG on 27.03.2016.
  */
-const WMFACTIONS = [Cryx, Cygnar, Khador, Menoth, Retribution, Mercenaries, Convergence];
-const HOFACTIONS = [Trollbloods, Skorne, Minions, Circle, Legion];
+const WMFACTIONS = ["Cryx", "Cygnar", "Khador", "Menoth", "Retribution", "Mercenaries", "Convergence"];
+const HOFACTIONS = ["Trollbloods", "Skorne", "Minions", "Circle", "Legion"];
 
-$("#system>li").is("Warmachine"){
-    for(i in WMFACTIONS) {
-        $('#faction').add('ul').html(i);
+
+$("#systemList li a").click(function() {
+    $(this).parents(".btn-group").find("#system").html($(this).text());
+});
+    var sysSelected = $("#system").text();
+
+    if (sysSelected == "Warmachine") {
+        for (i in WMFACTIONS)
+            $("#faction ul").append("<li>" + i + "</li>");
     }
+    else if (sysSelected == "Hordes") {
+        for (x in HOFACTIONS)
+            $("#faction ul").append("<li>" + x + "</li>");
+
+    }
+
+
+
+
+
+
     
-}
